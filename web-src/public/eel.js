aa6aed6d2d078458766ae6ca3665e5a5c5ebd1ed
tomnijhof <tom.nijhof@cytosmart.com>
2023-03-06 14:18:26 +0100

    remove unused eel mockups
    Signed-off-by: tomnijhof <tom.nijhof@cytosmart.com>

--- web-src/public/eel.js
+++ web-src/public/eel.js
@@ -1,12 +1,4 @@
 class Eel {
-  /* 
-  This  is for testing online.
-  Here are placeholders of the eel functions
-  so that the front-end can be tested with 'npm run dev'
-  */
-  hello_world() {
-    console.log("Hello from eel placeholder");
-  }
   ask_question(person_id, question) {
     /*
     Mockup ask_question for testing
@@ -15,7 +7,6 @@ class Eel {
       answer: `I am person ${person_id} and this is my answer`,
       question,
     };
-    console.log("ask_question", question);
     return (func) => func(greeting);
   }
 }
