a7999b387c5f6d8368d24d54b22a707ce8045635
tomnijhof <tom.nijhof@cytosmart.com>
2023-03-07 22:29:38 +0100

    first layout
    Signed-off-by: tomnijhof <tom.nijhof@cytosmart.com>

---
+++ web-src/src/components/Profile.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="flex w-full justify-center h-full">
+        <img src="../assets/person1_big.png" class="h-full overflow-hidden" alt="Person 1" />
+    </div>
+</template>
