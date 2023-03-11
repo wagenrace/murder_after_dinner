19146d739402c917014949ad96d365cf8ec6217f
tomnijhof <tom.nijhof@cytosmart.com>
2023-03-11 23:02:11 +0100

    formating
    Signed-off-by: tomnijhof <tom.nijhof@cytosmart.com>

--- Charactor_knowledges/all_persons.py
+++ Charactor_knowledges/all_persons.py
@@ -6,4 +6,11 @@ __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file
 with open(os.path.join(__location__, "general_knowledge.txt"), "r") as f:
     general_knowledge = f.read()
 
-all_persons = [Person("Jing", general_knowledge, "")]
+all_persons = [
+    Person("Amber", general_knowledge, ""),
+    Person("Saabir", general_knowledge, ""),
+    Person("Jabari", general_knowledge, ""),
+    Person("Jing", general_knowledge, ""),
+    Person("Feodora", general_knowledge, ""),
+    Person("Gael", general_knowledge, ""),
+]
