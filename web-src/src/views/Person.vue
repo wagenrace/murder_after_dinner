01c67b137bf4b88d51e386c4376067d5d700dc2d
tomnijhof <tom.nijhof@cytosmart.com>
2023-03-11 23:05:14 +0100

    save chat history
    Signed-off-by: tomnijhof <tom.nijhof@cytosmart.com>

--- web-src/src/views/Person.vue
+++ web-src/src/views/Person.vue
@@ -1,20 +1,40 @@
 <script setup>
-import { nextTick, onMounted, ref } from 'vue';
+import { nextTick, onMounted, ref, watch, computed } from 'vue';
 import Message from '../components/Message.vue';
 import Profile from '../components/Profile.vue'
 import { useRoute } from 'vue-router'
 
 const route = useRoute()
+const personId = computed(() => route.params.id)
+const personName = ref("")
+const personImageName = ref("")
 const input = ref(null)
 const messagesBox = ref(null)
+const answeredQuestions = ref([])
+const question = ref("")
 
 onMounted(() => {
     input.value.focus()
     messagesBox.value.scrollTop = messagesBox.value.scrollHeight
 })
 
-const answeredQuestions = ref([])
-const question = ref("")
+const _loadPerson = (_person) => {
+    personName.value = _person.name
+    answeredQuestions.value = _person.answeredQuestions
+    personImageName.value = _person.imageName
+    nextTick(() => {
+        if (messagesBox.value) messagesBox.value.scrollTop = messagesBox.value.scrollHeight
+    })
+
+}
+const loadPerson = () => {
+    eel.get_person(personId.value)(_loadPerson)
+}
+watch(personId, () => {
+    loadPerson()
+})
+
+
 const addMessage = (answeredQuestion) => {
     answeredQuestions.value.push(answeredQuestion)
     // Move to bottom of the chat when div is updated (nextTick)
@@ -33,7 +53,9 @@ const fillingUp = () => {
     eel.ask_question(route.params.id, "When did you leave the dinner?")(addMessage)
     eel.ask_question(route.params.id, "Germany is a Western European country with a landscape of forests, rivers, mountain ranges and North Sea beaches. It has over 2 millennia of history. Berlin, its capital, is home to art and nightlife scenes, the Brandenburg Gate and many sites relating to WWII. Munich is known for its Oktoberfest and beer halls, including the 16th-century Hofbräuhaus. Frankfurt, with its skyscrapers, houses the European Central Bank.")(addMessage)
 }
-fillingUp()
+
+loadPerson()
+
 </script>
 
 <template>
@@ -50,7 +72,7 @@ fillingUp()
             <button class="bg-red-500 p-2 rounded-full w-24" @click="fillingUp">Fill</button>
         </div>
         <div class="w-2/5 h-full bg-green-200">
-            <Profile />
+            <Profile :imageName="personImageName" />
         </div>
     </div>
 </template>
