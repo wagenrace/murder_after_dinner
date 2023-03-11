b0b394dc834c92b959ca5ff9f0df54901f82bf86
tomnijhof <tom.nijhof@cytosmart.com>
2023-03-11 23:05:22 +0100

    check profile
    Signed-off-by: tomnijhof <tom.nijhof@cytosmart.com>

--- web-src/src/components/Profile.vue
+++ web-src/src/components/Profile.vue
@@ -1,9 +1,11 @@
 <script setup>
-
+defineProps({
+    imageName: String
+})
 </script>
 
 <template>
     <div class="flex w-full justify-center h-full">
-        <img src="../assets/Jing.png" class="object-cover object-top w-full" alt="Portrait of Jing" />
+        <img :src="'/src/assets/' + imageName" class="object-cover object-top w-full" :alt="'Portrait of ' + imageName" />
     </div>
 </template>
